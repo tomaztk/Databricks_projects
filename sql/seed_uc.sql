90ff019b8e484e9cd7fb498a67805d4a4f159816
Tomaz Kastrun <tomaz.kastrun@gmail.com>
2025-08-15 13:49:26 +0200

    Update changes on schema and YML file

--- sql/seed_uc.sql
+++ sql/seed_uc.sql
@@ -1,18 +1,17 @@
 CREATE SCHEMA IF NOT EXISTS hive_metastore.demo;
 
-CREATE OR REPLACE TABLE hive_metastore.demo.hello_uc (
-  id   INT,
-  name STRING
-) COMMENT 'Seeded by GitHub Actions pipeline';
+CREATE OR REPLACE TABLE hive_metastore.demo.hello_github_uc (
+   id  INT ,
+   name STRING DEFAULT 'Hello UC from Github',
+   Insert_date TIMESTAMP  DEFAULT CURRENT_TIMESTAMP(),
+   year INT GENERATED ALWAYS AS (YEAR(Insert_date))
+) COMMENT 'Seeded by GitHub Actions pipeline'
+TBLPROPERTIES (
+'delta.feature.allowColumnDefaults' = 'supported', 
+'delta.columnMapping.mode' = 'name',
+'delta.enableDeletionVectors' = true,
+'delta.minReaderVersion' = '1', 
+'delta.minWriterVersion'='7');
 
-INSERT INTO hive_metastore.demo.hello_uc (id, name) VALUES
- (1,'one'),
- (2,'two'),
- (3,'three'),
- (4,'four'),
- (5,'five'),
- (6,'six'),
- (7,'seven'),
- (8,'eight'),
- (9,'nine'),
- (10,'ten');
+INSERT INTO hive_metastore.demo.hello_github_uc(id)
+SELECT 1 UNION SELECT 2 UNION SELECT 3 UNION SELECT 4
